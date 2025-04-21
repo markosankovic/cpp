8e0f315add8db093c9b60a914adf4880f29cbc61
Marko Sanković <sankovic.marko@gmail.com>
2025-04-21 16:12:58 +0200

    Add vcpkg as a submodule

--- apps/playground/playground.cc
+++ apps/playground/playground.cc
@@ -1,8 +1,13 @@
 #include "playground.h"
 
+#include <fmt/core.h>
+
 #include <iostream>
+#include <loguru/loguru.hpp>
 
 int main() {
   std::cout << "Playground app running!" << std::endl;
+  fmt::print("Hello from fmt!\n");
+  LOG_F(INFO, "Hello from loguru!");
   return 0;
 }
