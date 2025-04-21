c859ebf07246acf149f3457bf855f935408af661
Marko Sanković <sankovic.marko@gmail.com>
2025-04-21 23:04:17 +0200

    Remove fmt as a dependency

--- apps/playground/playground.cc
+++ apps/playground/playground.cc
@@ -1,13 +1,12 @@
 #include "playground.h"
 
-#include <fmt/core.h>
-
+#include <format>
 #include <iostream>
-#include <loguru/loguru.hpp>
+#include <loguru.hpp>
 
 int main() {
   std::cout << "Playground app running!" << std::endl;
-  fmt::print("Hello from fmt!\n");
+  std::cout << std::format("Hello from {}!", "format") << std::endl;
   LOG_F(INFO, "Hello from loguru!");
   return 0;
 }
